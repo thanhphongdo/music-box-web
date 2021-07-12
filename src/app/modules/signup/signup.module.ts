ad114b70e659e5b460f1079cd713107b16347102
huyit1999a <huyit1999a@gmail.com>
2021-07-12 13:55:01 +0700

    US-2-F1 Sign up page

--- src/app/modules/signup/signup.module.ts
+++ src/app/modules/signup/signup.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: SignupComponent,
+    canActivate: [],
+  },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ]
 })
 export class SignupModule { }
