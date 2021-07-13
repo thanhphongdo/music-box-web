db26b9ae2c43612ff7b87844e306f926540acd8a
huyit1999a <huyit1999a@gmail.com>
2021-07-13 12:01:12 +0700

    US-3-F1 login page master layout

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    canActivate: [],
+  },
+];
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ]
 })
 export class LoginModule { }
