72145461d1c6f03bb3e8108368dbf8c116105fd5
huyit1999a <huyit1999a@gmail.com>
2021-07-13 17:11:50 +0700

    US-3-F1 fix

--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+declare var $: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  formLogin!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      remember: ['']
+    });
   }
 
+  login() {
+    if (this.formLogin.invalid) {
+      return;
+    }
+    console.log(this.formLogin.value)
+    this.formLogin.reset();
+  }
 }
--- src/app/modules/signup/signup.module.ts
+++ src/app/modules/signup/signup.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
@@ -18,6 +19,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SignupModule { }
