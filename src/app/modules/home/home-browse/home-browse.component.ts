94459254861776ee7012e0a2600ad09d64e9d046
huyit1999a <huyit1999a@gmail.com>
2021-07-15 20:11:39 +0700

    US-7-F3 genre of mood

---
+++ src/app/modules/home/home-browse/home-browse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-browse',
+  templateUrl: './home-browse.component.html',
+  styleUrls: ['./home-browse.component.scss']
+})
+export class HomeBrowseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
